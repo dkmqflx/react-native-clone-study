8cc36ae4d281520892b8abad4e39999bb0f8e9f1
dkmqflx <dkmqflx@gmail.com>
2025-07-04 20:14:22 +0900

    11. StyleSheet와 react-native-safe-area-context

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,34 +1,43 @@
 import { usePathname, useRouter } from "expo-router";
-import { Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Index() {
   const router = useRouter();
   const pathname = usePathname();
 
-  console.log("pathname", pathname);
+  const insets = useSafeAreaInsets();
+  console.log("insets", insets); // 어느 정도의 간격을 띄워야 하는지를 알 수 있다
+
+  /**
+   * SafeAreaView 컴포넌트를 사용하면
+   * 상단과의 간격을 자동으로 조정해준다.
+   * 다만, 해당 컴포넌트가 작동되지 않는 휴대폰이 있는데 이 때 useSafeAreaInsets를 사용할 수 있다
+   */
 
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={[
+        styles.container,
+        { paddingTop: insets.top, paddingBottom: insets.bottom },
+      ]}
     >
-      <View>
-        <TouchableOpacity onPress={() => router.navigate(`/`)}>
-          <Text style={{ color: pathname === "/" ? "red" : "black" }}>
-            For you
-          </Text>
-        </TouchableOpacity>
-      </View>
+      <View style={styles.tabContainer}>
+        <View style={styles.tab}>
+          <TouchableOpacity onPress={() => router.navigate(`/`)}>
+            <Text style={{ color: pathname === "/" ? "red" : "black" }}>
+              For you
+            </Text>
+          </TouchableOpacity>
+        </View>
 
-      <View>
-        <TouchableOpacity onPress={() => router.navigate(`/following`)}>
-          <Text style={{ color: pathname === "/" ? "black" : "red" }}>
-            Following
-          </Text>
-        </TouchableOpacity>
+        <View style={styles.tab}>
+          <TouchableOpacity onPress={() => router.navigate(`/following`)}>
+            <Text style={{ color: pathname === "/" ? "black" : "red" }}>
+              Following
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
 
       <View>
@@ -51,3 +60,23 @@ export default function Index() {
     </View>
   );
 }
+
+/**
+ * inline style 대신 StyleSheet을 사용하는 이유:
+ * StyleSheet.create로 정의된 스타일은 앱이 로드될 때 또는 컴포넌트가 처음 마운트될 때 한 번만 파싱되고 최적화되어 네이티브 UI 레이어(브릿지)로 전송됩니다.
+ * 내부적으로 캐싱되며, 이후 렌더링 사이클에서는 이 캐시된 스타일의 ID만 참조하게 됩니다.
+ * 이는 런타임에 불필요한 계산이나 브릿지 통신을 줄여 앱의 성능을 향상시킵니다.
+ */
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  tabContainer: {
+    flexDirection: "row",
+  },
+  tab: {
+    flex: 1,
+    alignItems: "center",
+  },
+});
